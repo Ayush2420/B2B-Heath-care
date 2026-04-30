04e38ea28f699a5cf935f453291322c658bc6835
Ayush2420 <86476886+Ayush2420@users.noreply.github.com>
2026-04-30 19:53:17 +0530

    Update App.jsx changes

--- B2BHealthcare/src/App.jsx
+++ B2BHealthcare/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/Login/LoginPage";
+import LoginPage from "./pages/login/LoginPage";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import DashboardPage from "./pages/dasboard/DasboardPage.jsx";
